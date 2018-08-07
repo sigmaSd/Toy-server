fffe7771fc204cf0edc2a915af8e67e16887f2a1
Nbiba Bedis <bedisnbiba@gmail.com>
2018-08-07 16:01:48 +0100

    fix '..'

--- src/html.rs
+++ src/html.rs
@@ -9,13 +9,14 @@ crate fn append_dir(l: &[String], root: bool) {
     content.push_str("<html>\n");
     content.push_str("<body>\n<h1>Files list<h1>\n");
     content.push_str("<hr>\n<ul>\n");
+
     if !root {
-        content.push_str("<li><a href='../'>..</a></li>\n");
+        content.push_str(&get_parent(l.first()));
     }
 
     for link in l {
         if link.contains("dir_toy_server.html") {
-            continue
+            continue;
         }
         let mut name = link.clone();
         if name.ends_with('/') {
@@ -23,11 +24,35 @@ crate fn append_dir(l: &[String], root: bool) {
         }
 
         let idx = name.rfind('/').unwrap_or(0);
-        name.replace_range(..idx, "");
-        name = name.replace('/', "");
 
+        name.replace_range(..idx, "");
+
+        name = name.replace('/', "");
         content.push_str(&format!("<li><a href='/{}'>{}</a></li>\n", link, name));
     }
     content.push_str("</ul>\n<hr>\n</body>\n</html>\n");
     writeln!(html, "{}", &content);
+}
+fn get_parent(s: Option<&String>) -> String {
+    let result = match s {
+        Some(s) => {
+            let mut s = s.to_string();
+            if s.ends_with('/') {
+                s.pop();
+            }
+            let idx = s.rfind('/').unwrap_or(0);
+            s.replace_range(idx.., "");
+            match s.rfind('/') {
+                Some(idx) => {
+                    s.replace_range(idx.., "");
+                    s.insert(0, '/');
+                }
+                None => s = ".".to_string(),
+            }
+            s
+        }
+        None => ".".to_string(),
+    };
+
+    format!("<li><a href='{}'>..</a></li>\n", &result)
 }
