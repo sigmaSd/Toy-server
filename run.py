1f85e672b25a90e3f6ece307a87f771e4f5b35af
Nbiba Bedis <bedisnbiba@gmail.com>
2018-08-07 13:51:29 +0100

    fix python script

--- run.py
+++ run.py
@@ -1,14 +1,18 @@
-import subprocess 
+import subprocess
 import sys
+import os
 
-print (sys.argv)
-
-command = ["cargo","run"] + sys.argv[1:]
+dir_path = os.path.dirname(os.path.realpath(__file__))
+cargo_toml = dir_path + "/Cargo.toml"
+command = ["cargo", "run", "--release",
+           "--manifest-path", cargo_toml] + sys.argv[1:]
 
 try:
     subprocess.run(command)
 except:
-    subprocess.run(["rm","dir_toy_server.html"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-    
+    subprocess.run(["rm", "dir_toy_server.html"],
+                   stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
 finally:
-    subprocess.run(["rm","dir_toy_server.html"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    subprocess.run(["rm", "dir_toy_server.html"],
+                   stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
